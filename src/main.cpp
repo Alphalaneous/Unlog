21cc4760ea6dfc8703ab92c1553851045a88cb54
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2025-05-23 15:26:24 -0400

    Better hooking

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,32 @@
 #include <Geode/Geode.hpp>
 #include <Geode/ui/GeodeUI.hpp>
 #include "ModListPopup.hpp"
+#include <alphalaneous.alphas_geode_utils/include/NodeModding.h>
 
 using namespace geode::prelude;
 
 #include <Geode/modify/CCLayer.hpp>
 
-class ModsLayer : public CCNode {};
+class $nodeModify(ModsLayerExt, ModsLayer) {	
+    void modify() {
+		bool geodeTheme = Loader::get()->getLoadedMod("geode.loader")->getSettingValue<bool>("enable-geode-theme");
+		if (CCMenu* actionsMenu = typeinfo_cast<CCMenu*>(getChildByID("actions-menu"))) {
 
-class $modify(ModsLayerExt, CCLayer) {	
-    bool init() {
-    	if (!CCLayer::init()) return false;
-		if (typeinfo_cast<ModsLayer*>(this)) {
-			queueInMainThread([this] {
-				bool geodeTheme = Loader::get()->getLoadedMod("geode.loader")->getSettingValue<bool>("enable-geode-theme");
-				if (CCMenu* actionsMenu = typeinfo_cast<CCMenu*>(getChildByID("actions-menu"))) {
+			CCSprite* spr = CircleButtonSprite::createWithSprite(
+				"terminal.png"_spr, .85f,
+				(geodeTheme ? CircleBaseColor::DarkPurple : CircleBaseColor::Green)
+			);
+			spr->setScale(0.8f);
 
-					CCSprite* spr = CircleButtonSprite::createWithSprite(
-						"terminal.png"_spr, .85f,
-						(geodeTheme ? CircleBaseColor::DarkPurple : CircleBaseColor::Green)
-					);
-					spr->setScale(0.8f);
-
-					CCMenuItemSpriteExtra* logsBtn = CCMenuItemExt::createSpriteExtra(
-						spr, [](CCNode*) {
-							ModListPopup::create()->show();
-						}
-					);
-					logsBtn->setID("mods-logs-button");
-					actionsMenu->addChild(logsBtn);
-					actionsMenu->updateLayout();
+			CCMenuItemSpriteExtra* logsBtn = CCMenuItemExt::createSpriteExtra(
+				spr, [](CCNode*) {
+					ModListPopup::create()->show();
 				}
-			});
+			);
+			logsBtn->setID("mods-logs-button");
+			actionsMenu->addChild(logsBtn);
+			actionsMenu->updateLayout();
 		}
-		return true;
 	}
 };
 
